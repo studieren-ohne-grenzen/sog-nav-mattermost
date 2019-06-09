ba5a93ba58aa4e7bfa090183b0d0d63b7a906ed6
linus.heck@rwth-aachen.de <linus@lintop.localdomain>
2019-06-09 12:39:59 +0200

    added nav as submodule

---
+++ README.md
@@ -0,0 +1,8 @@
+## How to build this
+
+1. Install `npm` (oh no)
+2. Run `npm install` (oh noo)
+3. (Manually install everything else that npm might complain about not having)
+4. Run `make`
+
+Compiled release will be `sog-nav.tar.gz`.
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,24 @@
 import "resources/nav/index.js"
 
+// https://stackoverflow.com/questions/1409225/changing-a-css-rule-set-from-javascript
+function getCSSRule(ruleName) {
+    ruleName = ruleName.toLowerCase();
+    var result = null;
+    var find = Array.prototype.find;
+
+    find.call(document.styleSheets, styleSheet => {
+        result = find.call(styleSheet.cssRules, cssRule => {
+            return cssRule instanceof CSSStyleRule 
+                && cssRule.selectorText.toLowerCase() == ruleName;
+        });
+        return result != null;
+    });
+    return result;
+}
+
 class SOGNavPlugin {
     initialize(registry, store) {
+        getCSSRule(".SOGNav__MenuItem").style.margin = "8px";
     }
 }
 
